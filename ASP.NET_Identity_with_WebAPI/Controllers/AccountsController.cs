0ed1f75b6aa1d94744fcea8bec1cf06d5b4af0b5
bsokat <barissokat@gmail.com>
2017-03-15 12:52:07 +0300

    Create the Accounts Controller

---
+++ ASP.NET_Identity_with_WebAPI/Controllers/AccountsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASP.NET_Identity_with_WebAPI.Controllers
+{
+    [RoutePrefix("api/accounts")]
+    public class AccountsController : BaseApiController
+    {
+        [Route("users")]
+        public IHttpActionResult GetUsers()
+        {
+            return Ok(this.AppUserManager.Users.ToList().Select(u => this.TheModelFactory.Create(u)));
+        }
+
+        [Route("user/{id:guid}", Name = "GetUserById")]
+        public async Task<IHttpActionResult> GetUser(string Id)
+        {
+            var user = await this.AppUserManager.FindByIdAsync(Id);
+
+            if (user != null)
+            {
+                return Ok(this.TheModelFactory.Create(user));
+            }
+
+            return NotFound();
+
+        }
+
+        [Route("user/{username}")]
+        public async Task<IHttpActionResult> GetUserByName(string username)
+        {
+            var user = await this.AppUserManager.FindByNameAsync(username);
+
+            if (user != null)
+            {
+                return Ok(this.TheModelFactory.Create(user));
+            }
+
+            return NotFound();
+
+        }
+    }
+}
---
+++ ASP.NET_Identity_with_WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NET_Identity_with_WebAPI.MvcApplication" Language="C#" %>
