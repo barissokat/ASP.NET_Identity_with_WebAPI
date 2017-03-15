612a8dd254ee54fa44a0be72dfc63071f1d0f033
bsokat <barissokat@gmail.com>
2017-03-15 09:59:44 +0300

    Add the User Manager Class

---
+++ ASP.NET_Identity_with_WebAPI/Infrastructure/ApplicationUserManager.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_Identity_with_WebAPI.Infrastructure
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager(IUserStore<ApplicationUser> store) : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+        {
+            var appDbContext = context.Get<ApplicationDbContext>();
+            var appUserManager = new ApplicationUserManager(new UserStore<ApplicationUser>(appDbContext));
+
+            return appUserManager;
+        }
+    }
+}
